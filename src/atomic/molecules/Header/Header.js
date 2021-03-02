96e6eac00db7d08e6fdf600e3db868f668cb2761
dstph <genyadstph@gmail.com>
2021-03-02 15:09:09 +0300

    second

--- src/atomic/molecules/Header/Header.js
+++ src/atomic/molecules/Header/Header.js
@@ -1,10 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Header = ({array}) => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 900px)'
+  })
+  const isTabletOrMobile = useMediaQuery({ query: '(min-device-width: 600px)' })
+  const isMobileDevice = useMediaQuery({
+    query: '(max-device-width: 599yarpx)'
+  })
   const [date, setDate] = useState(new Date)
   const currentDate = date.toLocaleString().slice(1,17)
   return(
-    <div className='header-container'>
+    <div className={isDesktopOrLaptop ?  'header-container' : isTabletOrMobile ? 'header-container-tablet' : 'header-container-mobile'}>
       <div className='length'>Quantity : {array.length}</div>
       <div className='date'>Current date : {currentDate}</div>
     </div>
--- src/atomic/organisms/MyGrid/MyGrid.js
+++ src/atomic/organisms/MyGrid/MyGrid.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'; 
+import { useMediaQuery } from 'react-responsive';
 import Img from '../../atoms/Img/Img';
 import {images} from '../../../images';
 import Header from '../../molecules/Header/Header';
-import Modal from '../../molecules/Modal/Modal'
+import Modal from '../../molecules/Modal/Modal';
 
 
 const MyGrid = () => {
-  const [pictures, setPictures] = useState(images);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 900px)'
+  })
+  const isTabletOrMobile = useMediaQuery({ query: '(min-device-width: 600px)' })
+  const isMobileDevice = useMediaQuery({
+    query: '(max-device-width: 599yarpx)'
+  })
+  const [pictures, setPictures] = useState(localStorage.getItem('pictures')
+  ? JSON.parse(localStorage.getItem('pictures'))
+  : images);
   const [flag, setFlag] = useState(false)
   const [choosenId, setChoosenId] = useState('')
-  const [choosenPicture, setChoosenPicture] = useState({id:'rap'})
+  const [choosenPicture, setChoosenPicture] = useState({})
   const handleDeletePicture = (id)=> {
     setPictures(pictures.filter(el=>el.id!==id));
 	}
@@ -17,17 +27,18 @@ const MyGrid = () => {
   const handlerChoosenPicture = (id) => {    
     setChoosenId(id)
     setFlag(!flag)
-   
   }
+  useEffect(() => {
+    localStorage.setItem('pictures', JSON.stringify(pictures))
+  }, [pictures])
   useEffect(()=>{
-    setChoosenPicture(pictures.find(el=>el.id==choosenId));
+    setChoosenPicture(pictures.find(el=>el.id===choosenId));
     console.log(choosenPicture)
   },[choosenId, flag])
   return(
     <>
-     
       <Header array={pictures} />
-      <div className='my-grid'>
+      <div className={isDesktopOrLaptop ?  'my-grid' : isTabletOrMobile ? 'my-grid-tablet' : 'my-grid-phone'}>
         {
           pictures.map((data,key)=>
             <Img img={data.logo} id={data.id} key={key} handlerChoosenPicture={handlerChoosenPicture} handler={handleDeletePicture} />
@@ -37,6 +48,7 @@ const MyGrid = () => {
       {
         flag && choosenPicture && <Modal choosenPicture={choosenPicture} handlerChoosenPicture={handlerChoosenPicture} />
       }
+      <button className={isDesktopOrLaptop ?  'btn' : isTabletOrMobile ? 'btn-tablet' : 'btn-phone'} onClick={()=>setPictures(images)}>restore</button>
     </>  
   )
   }
